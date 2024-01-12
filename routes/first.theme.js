719bd8f3ee448f62dd97ae48b577447aae2e468b
Mark <mark@MacBook-Pro-Mark.local>
2024-01-12 16:03:50 +0300

    done

--- routes/first.theme.js
+++ routes/first.theme.js
@@ -1,21 +1,19 @@
-
 const router = require('express').Router()
+
 const FirstTheme = require('../components/FirstTheme')
+
 
 const {Question} = require('../db/models') // берем вопросы с базы данных
 
-
 router.get('/', async (req, res) => {
-  try {
-    const questions = await Question.findAll();
-    const html = res.renderComponent(FirstTheme, {
-      title: 'Questions Page',
-      questions,
-    });
-    res.send(html);
-  } catch ({ message }) {
-    res.json({ message });
-  }
-});
+   try {
+    const questions = await Question.findAll()
+    const html = res.renderComponent(FirstTheme, {title: 'Questions Page', questions, theme_id: 1})
+    res.send(html)
+   } catch ({message}) {
+    res.json({message})
+   }
+})
 
-module.exports = router;
+
+module.exports = router
