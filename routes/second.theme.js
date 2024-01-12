ffc9dc34a7dfe79a5be3292f339c267872627ad3
Mark <mark@MacBook-Pro-Mark.local>
2024-01-12 14:10:02 +0300

    add.js

--- routes/second.theme.js
+++ routes/second.theme.js
@@ -1,23 +1,21 @@
-// const router = require('express').Router()
 
-// const HeroPage = require('../components/HeroPage')
-// const HeroesListPage = require('../components/HeroesListPage')
+const router = require('express').Router()
+const FirstTheme = require('../components/FirstTheme')
 
-// const {Hero} = require('../db/models') // берем героев с базы данных
+const {Question} = require('../db/models') // берем вопросы с базы данных
 
-// router.get('/', async (req, res) => {
-//    try {
-//     const heroes = await Hero.findAll()
-//     const html = res.renderComponent(HeroesListPage, {title: 'Heroes Page', heroes})
-//     res.send(html)
-//    } catch ({message}) {
-//     res.json({message})
-//    }
-// })
 
-// router.get('/:heroId', (req, res) => {
-//     const html = res.renderComponent(HeroPage, {title: 'Hero Page', heroId})
-//     res.send(html)
-// })
+router.get('/', async (req, res) => {
+  try {
+    const questions = await Question.findAll();
+    const html = res.renderComponent(FirstTheme, {
+      title: 'Questions Page',
+      questions,
+    });
+    res.send(html);
+  } catch ({ message }) {
+    res.json({ message });
+  }
+});
 
-// module.exports = router
+module.exports = router;
