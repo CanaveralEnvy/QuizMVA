08f4c266e4d7034f6b9c3b6e5eb163bc0dab071b
AnnaKalami <Vampire1621@gmail.com>
2024-01-12 11:31:18 +0300

    new Naw

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,16 @@
 const React = require('react');
+const NavBar = require('./NavBar')
 
 function Layout({title, children }) {
   return (
     <html lang="en">
       <head>
         <title>{title}</title>
-        <link rel="stylesheet" href="/css/style.css" />
+        {/* <link rel="stylesheet" href="/css/style.css" /> */}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+        crossorigin="anonymous"></link>
         <script src="/js/script.js" /> 
       </head>
       <body>
