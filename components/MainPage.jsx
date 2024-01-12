6d56106698852edc09a84e00850abf6c48743b78
CanaveralEnvy <oldelevennn@gmail.com>
2024-01-12 11:29:44 +0300

    db fixed

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -2,10 +2,16 @@ const React = require('react')
 const Layout = require('./Layout')
 
 
-function MainPage({ title }) {
+function MainPage({ title, themes }) {
     return (
      <Layout title={title}> 
-     <h1>Main Page</h1>
+     <h1>Heroes</h1>
+            {themes.map((theme) =>(
+                 <div key={theme.id}>
+                <h5>{theme.name}</h5>
+                <h5>{theme.description}</h5>
+                <img src={theme.img} alt="hero" />
+            </div>) )}
     </Layout> 
      );
     };
